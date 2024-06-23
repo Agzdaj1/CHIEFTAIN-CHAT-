67b2f87b9282cdc9bfe415f8039caf7763444709
Agzdaj1 <146616861+Agzdaj1@users.noreply.github.com>
2024-06-23 12:46:58 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,12 @@
 <?php
-$host = 'db5015991272.hosting-data.io';
-$db = 'dbs13030850';
-$user = 'dbu669452';
-$pass = 'viatuzangu@098';
-$port = 3306;
-
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4;port=$port";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
+$dsn = 'mysql:host=db5015991272.hosting-data.io;port=3306;dbname=dbs13030850';
+$username = 'dbu669452';
+$password = 'viatuzangu@098';
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
+    die("DB ERROR: ". $e->getMessage());
 }
 ?>
